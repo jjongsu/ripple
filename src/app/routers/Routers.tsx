58c1720e80c4e14f3df5120f48a7aa1198a7c607
jongsu <hjs1002@jei.com>
2024-07-25 11:22:52 +0900

    🐛 Fix : outlet 적용

--- src/app/routers/Routers.tsx
+++ src/app/routers/Routers.tsx
@@ -1,18 +1,43 @@
 import { lazy } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, Outlet, RouteObject, RouterProvider } from 'react-router-dom';
 
 const Ripple1Page = lazy(() => import('@pages/ripple1'));
 const Ripple2Page = lazy(() => import('@pages/ripple2'));
 
 export default function Router() {
-    return (
-        <BrowserRouter basename={'/ripple/'}>
-            <Routes>
-                <Route path='/1' element={<Ripple1Page />} />
-                <Route path='/2' element={<Ripple2Page />} />
+    const routes: RouteObject[] = [
+        {
+            path: '/ripple/',
+            element: (
+                <div className='bg-black'>
+                    메인
+                    <Outlet />
+                </div>
+            ),
+            children: [
+                {
+                    path: '/ripple/1',
+                    element: <Ripple1Page />
+                },
+                {
+                    path: '/ripple/2',
+                    element: <Ripple2Page />
+                }
+            ]
+        }
+    ];
 
-                <Route path='*' element={<div className='bg-black'>메인</div>} />
-            </Routes>
-        </BrowserRouter>
-    );
+    const router = createBrowserRouter(routes);
+
+    return <RouterProvider router={router} />;
+    // return (
+    //     <BrowserRouter basename={'/ripple/'}>
+    //         <Routes>
+    //             <Route path='/1' element={<Ripple1Page />} />
+    //             <Route path='/2' element={<Ripple2Page />} />
+
+    //             <Route path='*' element={<div className='bg-black'>메인</div>} />
+    //         </Routes>
+    //     </BrowserRouter>
+    // );
 }
