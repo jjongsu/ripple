9e87ae38bc826a7515bcd388563c5e484c4fdfa7
jongsu <hjs1002@jei.com>
2024-07-25 11:25:15 +0900

    🐛 Fix : basename

--- src/app/routers/Routers.tsx
+++ src/app/routers/Routers.tsx
@@ -7,7 +7,7 @@ const Ripple2Page = lazy(() => import('@pages/ripple2'));
 export default function Router() {
     const routes: RouteObject[] = [
         {
-            path: '/ripple/',
+            path: '/',
             element: (
                 <div className='bg-black'>
                     메인
@@ -16,18 +16,18 @@ export default function Router() {
             ),
             children: [
                 {
-                    path: '/ripple/1',
+                    path: '/1',
                     element: <Ripple1Page />
                 },
                 {
-                    path: '/ripple/2',
+                    path: '/2',
                     element: <Ripple2Page />
                 }
             ]
         }
     ];
 
-    const router = createBrowserRouter(routes);
+    const router = createBrowserRouter(routes, { basename: import.meta.env.BASE_URL });
 
     return <RouterProvider router={router} />;
     // return (
